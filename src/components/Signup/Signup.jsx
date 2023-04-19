fb630d506d3cc6a8ab5c14e0356994c77ae3ce50
monier87 <carlos.monier87@gmail.com>
2023-04-19 15:20:03 -0400

    Third commit

--- src/components/Signup/Signup.jsx
+++ src/components/Signup/Signup.jsx
@@ -8,6 +8,33 @@ import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth"
 export function Signup(){
   const navigate=useNavigate();
   const [values, setvalues] = useState({name:"", email: "", pass:""});
-  
-  return(<h1>Registro</h1>)
+  const [errorMsg, setErrorMsg]= useState([]);
+  const [submitButtonDisabled, setsubmitButtonDisabled]= useState(false)
+
+  const registro =()=>{
+    if(!values.name || !values.email || !values.pass ){
+      setErrorMsg("Llene todos los campos")
+      return;
+    }
+    setErrorMsg("")
+    setsubmitButtonDisabled(true)
+    createUserWithEmailAndPassword(auth,values.email,values.pass).then( async (res)=>{setsubmitButtonDisabled(false);
+      const user= res.user;
+      await updateProfile(user, {
+        displayName: values.name,
+      });
+      navigate("/");      
+    })
+    .catch((err)=>{
+      setsubmitButtonDisabled(false);
+      setErrorMsg(err.message);
+    });
+  };
+  return(<div className={styles.container}>
+    <div className={styles.innerBox}>
+      <h1 className={styles.heading}>Registro</h1>
+
+    </div>
+
+  </div>)
 }
